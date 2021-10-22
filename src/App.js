986a54312040d52190129f4f0deae6686ea60134
SeungJunii <ssj7410@gmail.com>
2021-10-22 11:49:17 +0900

    Array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import styled from '@emotion/styled';
-import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
 import React, { Component } from 'react';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          ScrollToBottom
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
